aa387d8e5141dcd54179308fcbd22b62879ea254
Siku Mahayi <smahayi31@gmail.com>
2022-07-21 14:24:51 +0200

    Added and documented Task4()

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -16,5 +16,23 @@ public class SimpleFunctions {
         }
         return sum;
     }
+
+    /**
+     * Multiplies the elements in the first list in sequential order, with the elements of 
+     * another list in reversing order.
+     * @param a: the list to be multiplied sequentially
+     * @param b: the list to be multiplied in reverse
+     * @return the products of these lists
+     */
+    public List<Integer> Task4(List<Integer> a, List<Integer> b) {
+        if (a.size() != b.size()) return null;
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < a.size(); i++) {
+            result.add(a.get(i) * b.get(b.size() - i));
+        }
+
+        return result;
+    }
 }
 
